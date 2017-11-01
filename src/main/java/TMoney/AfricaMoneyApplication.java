145495f988e4ef078ce3811cde0de7e06e9a2552
ipsis <ipsis@THIERNALIOU>
2017-11-01 21:28:42 +0100

    Création du Projet

---
+++ src/main/java/TMoney/AfricaMoneyApplication.java
@@ -0,0 +1,12 @@
+package TMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricaMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricaMoneyApplication.class, args);
+	}
+}
